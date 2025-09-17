d2a24bdb512bc1160036d3d046c3fd94e77377d3
Cameron Earls <cameron_earls@berkeley.edu>
2025-09-17 13:24:23 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,69 @@
+
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+#Git is the engine that powers everything, Github is the website you can use to access it, Gitbash is how windows users can acess
+
+# 2) What’s the difference between the terminal and the command line?
+# The commmand line is where you type commands and the terminal is how you access that command line
+
+
+# 3) How does Windows PowerShell differ from Git Bash?
+# Powershell is windows own commans tool and git bash is the tool git provides to adapt windows commands to mac
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+# Anaconda is an entire library for data science that includes conda and python, conda installs and manages things, python is a programming language
+
+# 5) What is VS Code? 
+# a free code editor developed by microsoft that allows users to write and debug code
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+# Jupityr notebook is an app used for writing and mixing code, jupyter lab is a more powerful version that allows you to open multiple notebooks or files at once
+
+# 7) What does ~/ mean?
+# Its shorthand for your home directory
+# 8) What’s the difference between an absolute path and a relative path?
+# The absolute path is the full adress of where you want to go and the relative path is the directions from where you are now
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+# cd ..
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+# It would delete your current directory
+
+# 12) What do the following commands do?
+# git add
+# It saves your changes to the staging area
+# git commit
+#Adds the changes made in the staging area and makes it a new snapshot
+# git push
+# sends your changes from a local repository to a remote one
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+# git add . adds the files in the current directory and file just choses a specific file to add
+
+# 14) What do "git status" and "git log -1" do? 
+# status shows the current state of your directory and staging area, git log -1 shows the most recent commit
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+# Cloning makes a copy of a remote repository, pulling from it updates your local repository with changes made from the remote repo
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+#The most frustrating challenge I had in this class was I didnt know how to work nano and how to submit it but I got help from someone in the class that told me to press enter instead of tab
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+# What are the most common tasks that people use python for in general science research
+
+# 18) Tell me a fun fact!
+# I love playing beach volleyball
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(5%2)
+# I like the remainder command because I feel like we rarely use it in math
